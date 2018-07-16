ba3c2dff24117bf3678839386d27b659866cf49b
raghavthakur <raghav.thakur.rt@gmail.com>
2018-07-15 22:54:08 -0700

    added Debug.Log statements

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,7 +7,9 @@ public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Welcome to number wizard");
-
+        Debug.Log("Pick a number");
+        Debug.Log("The highest number you can choose is 1000");
+        Debug.Log("The lowest number you can choose is 1");
 	}
 	
 	// Update is called once per frame
