abdc9c7a7cf620aa115d3697ca49684b202fbb6c
raghavthakur <raghav.thakur.rt@gmail.com>
2018-07-15 23:37:35 -0700

    added player input in Update method

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -13,10 +13,21 @@ public class NumberWizard : MonoBehaviour {
         Debug.Log("Pick a number");
         Debug.Log("The highest number you can choose is: " + maxGuess);
         Debug.Log("The lowest number you can choose is: " + minGuess);
+
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push UP = higher, Push DOWN = Lower, Push ENTER = Correct");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("Enter key was pressed.");
+        }
+    }
 }
