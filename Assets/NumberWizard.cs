6e8bcd282cbcecb9f790c79568a5b4f146d877b4
raghavthakur <raghav.thakur.rt@gmail.com>
2018-07-15 23:02:50 -0700

    added min and max number variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,10 +6,13 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        int maxGuess = 1000;
+        int minGuess = 1;
+
         Debug.Log("Welcome to number wizard");
         Debug.Log("Pick a number");
-        Debug.Log("The highest number you can choose is 1000");
-        Debug.Log("The lowest number you can choose is 1");
+        Debug.Log("The highest number you can choose is: " + maxGuess);
+        Debug.Log("The lowest number you can choose is: " + minGuess);
 	}
 	
 	// Update is called once per frame
